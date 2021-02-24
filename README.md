20a349c9ddff1faa35ae2335353421878b004c36
KUD <kimud6003@gmail.com>
2021-02-25 01:16:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# nginx,nodejs,mysql 도커
+
+## 사용법
+
+> docker-compose up -d 
+
+- http://localhost:8081/
---
+++ node/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const config = {
+    host: 'mysql',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const createTable = `CREATE TABLE IF NOT EXISTS people(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`
+
+connection.query(createTable);
+const sql = `INSERT INTO people (name) values ('dongjun')`
+
+connection.query(sql)
+
+app.get('/', (_req, res) => {
+    connection.query('SELECT name FROM people', (error, people, _fields) => {
+        if (error) throw error;
+
+        peopleNames = people.map(person => `<li>${person.name}</li>`).join()
+
+        let body = `<h1>test</h1>`
+        body += `<ul>${peopleNames}</ul>`
+
+        res.send(body)
+    });
+})
+
+const server = app.listen(port, () => {
+    console.log(`Port ${port}`)
+})
+
+process.on('SIGTERM', () => {
+    console.log('Close db connection')
+    connection.end()
+})
